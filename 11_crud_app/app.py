4dfb93392c57f27c9ae9f4b15e57eb2c00b879c1
kiranmaichinthapanti <kiranmaichinthapanti98@gmail.com>
2025-10-17 09:41:15 +0530

    26th Sep - Student Management System - 2

--- 11_crud_app/app.py
+++ 11_crud_app/app.py
@@ -17,14 +17,14 @@ while True:
     print("2 - Update Student")
     print("3 - Delete Student")
     print("4 - List Student")
-    print("5 - Exit Student")
+    print("5 - Exit System")
 
-    choice = input("ENter Choice (1-5): ")
+    choice = input("Enter Choice (1-5): ")
 
     # student add
     if choice == "1":
         print("Adding Student Logic")
-        student_id = int(input("Enter ID: "))
+        student_id = input("Enter ID: ")
         if student_id in students:
             print("ID Already Exists, Try with different ID")
         else:
@@ -32,31 +32,96 @@ while True:
 
             # list for scores
             scores = []
-            score_input = input("Enter Score or type done: ")
-            if score_input == "done":
-                break
-            if score_input.isdigit():
-                score = int(score_input)
-                if 0 <= score <= 100:
-                    scores.append(score_input)
+            while True:
+                score_input = input("Enter Score or type done: ")
+                if score_input == "done":
+                    break
+                if score_input.isdigit():
+                    score = int(score_input)
+                    if 0 <= score <= 100:
+                        scores.append(score)
+                    else:
+                        print("Score Should be 0-100 ")
                 else:
-                    print("Score Should be 0-100 ")
-            else:
-                print("Score should be Numbers Only")
+                    print("Score should be Numbers Only")
+
+            # set of skills
+            skills = set()
+            while True:
+                skill_input = input("Enter Skill or type done: ")
+                if skill_input == "done":
+                    break
+                skills.add(skill_input.title())
+
+            # save the student details
+            students[student_id] = {
+                "name": name,
+                "scores": scores,
+                "skills": skills
+            }
+            print("Student Saved")
+
+            # for our confirmation
+            print(students)
 
     elif choice == "2":
         print("Updating Student Logic")
+        student_id = input("Enter ID To Update: ")
+        if student_id in students:
+            new_name = input("Enter New Name: ").title()
+            students[student_id]["name"] = new_name
+            print("Student Name Updated")
+        else:
+            print("ID Doesn't Exist to Update")
+
+        # for our confirmation
+        print(students)
+
     elif choice == "3":
         print("Deleting Student Logic")
+        student_id = input("Enter ID To Delete: ")
+        if student_id in students:
+            remove_id = students.pop(student_id)
+            print("student deleted: ",remove_id)
+        else:
+            print("ID is not Exist to delete")
+
+        # for our confirmation
+        print(students)
+
     elif choice == "4":
         print("Listing Students Logic")
+        for sid, data in students.items():
+            name = data["name"]
+            scores = data["scores"]
+
+            avg = sum(scores)/len(scores)
+            high_score = max(scores)
+            low_score = min(scores)
+
+            skills = data["skills"]
+            skills_count = len(skills)
+
+            print("="*50)
+            print("STUDENT DETAILS")
+            print("="*50)
+
+            print(f"ID: {sid}")
+            print(f"NAME : {name}")
+            print(f"ALL SCORES : {scores}")
+            print(f"AVG SCORE : {avg}")
+            print(f"HIGH SCORE : {high_score}")
+            print(f"LOWEST SCORE : {low_score}")
+            print(f"ALL SKILLS : {skills}")
+            print(f"NO OF SKILLS : {skills_count}")            
+
     elif choice == "5":
         print("Exit System")
+        print("="*50)
+        print("CONTACT ADMIN FOR MORE INFORMATION")
+        print(f"ADMIN CONTACT NO: {ADMIN_INFO[0]}")
+        print(f"ADMIN EMAIL ID: {ADMIN_INFO[1]}")
+        print("="*50)
+        break
     else:
         print("Invalid Option, Only Select (1-5)")
-
-
-
-
-
-
