a37d5f9d12422bb7f59776f0401d059b7fe17bb6
kiranmaichinthapanti <kiranmaichinthapanti98@gmail.com>
2025-10-20 15:21:56 +0530

    7th Oct - Modules

---
+++ 13_modules/mathprofile.py
@@ -0,0 +1,16 @@
+# Custom Module for Math & Profile info
+
+name = "kiran"
+email = "kiran@gmail.com"
+
+def profile():
+    return f"Hi {name} and your email is {email}"
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
