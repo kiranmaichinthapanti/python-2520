ff005cd6ec35497589dae39d32c956147ed10d4b
kiranmaichinthapanti <kiranmaichinthapanti98@gmail.com>
2025-10-28 18:40:43 +0530

    25th Oct - Polymorphism

--- 06_strings/practice.py
+++ 06_strings/practice.py
@@ -1,1 +1,23 @@
 print("mango" and "apple")
+
+class A:
+    def test1(self):
+        print("method named test1 of A called")
+    
+class B(A):
+    def test1(self):
+        print("method named test1 of B called")
+
+class C(A):
+    def test1(self):
+        print("method named test1 of C called")
+
+class D(B,A):
+    def test2(self):
+        print("method named test1 of D called")
+
+obj = D()
+obj.test1()
+
+print("afzal",end="@")
+print("CodingNinjas")
